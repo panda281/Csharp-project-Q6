4f18c37085514ed6e963092b87c185e92ac7045f
Abinet Tamiru <Abinettamiru@outlook.com>
2022-04-30 14:19:03 +0300

    Add project files.

---
+++ Csharp project Q6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_project_Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please insert your age: ");
+            string x = Console.ReadLine();
+            int age = Int32.Parse(x) + 10;
+            Console.WriteLine("your age after 10 years is: " + age);
+        }
+    }
+}
